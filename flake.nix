a28fb9c8f2bae7fea47ed60bb3ee823552201508
Ahnaf Rafi <ahnaf.al.rafi@gmail.com>
2025-05-29 16:40:27 -0400

    Add some basic programs, remap capslock to ctrl.

--- flake.nix
+++ flake.nix
@@ -3,55 +3,109 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
-
-    # Optional: Declarative tap management
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
+    homebrew-core = { url = "github:homebrew/homebrew-core"; flake = false; };
+    homebrew-cask = { url = "github:homebrew/homebrew-cask"; flake = false; };
   };
 
-  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = [];
+  outputs = {
+    self, nixpkgs, nix-darwin, home-manager,
+    nix-homebrew, homebrew-core, homebrew-cask, ...
+    }:
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+    let
+      configuration = { pkgs, config, ... }: {
+        nixpkgs.config.allowUnfree = true;
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = [
+          pkgs.neovim
+          pkgs.mkalias
+          pkgs.wezterm
+          # pkgs.tmux
+        ];
+        fonts.packages = [
+          pkgs.julia-mono
+        ];
+        nix.settings.experimental-features = "nix-command flakes";
+        programs.zsh.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+        system.keyboard.remapCapsLockToControl = true;
 
-      # Used for backwards compatibility, please read the changelog before
-      # changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+        system.activationScripts.applications.text = let
+          env = pkgs.buildEnv {
+            name = "system-applications";
+            paths = config.environment.systemPackages;
+            pathsToLink = "/Applications";
+          };
+        in
+          pkgs.lib.mkForce ''
+  # Set up applications.
+  echo "setting up /Applications..." >&2
+  rm -rf /Applications/Nix\ Apps
+  mkdir -p /Applications/Nix\ Apps
+  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+  while read -r src; do
+    app_name=$(basename "$src")
+    echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+  done
+          '';
+
+        # Used for backwards compatibility, please read the changelog before
+        # changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+      };
+    in {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#ECON-DYR5QNPF6C
+      darwinConfigurations."ECON-DYR5QNPF6C" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "ahnafrafi";
+
+              # Optional: Declarative tap management
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+              };
+
+              # Optional: Enable fully-declarative tap management
+              #
+              # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+              mutableTaps = false;
+            };
+          }
+        ];
+      };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#ECON-DYR5QNPF6C
-    darwinConfigurations."ECON-DYR5QNPF6C" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
 }
