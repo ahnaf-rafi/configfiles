fa7b5be21e2f348e0544650ce46aae58ff9c1512
Ahnaf Rafi <ahnaf.al.rafi@gmail.com>
2025-12-27 00:33:05 -0500

    Add basic modular flake setup.

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Ahnaf Rafi's configurations for NixOS and nix-darwin";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
+  };
+
+  outputs = {
+    self, nixpkgs, home-manager, emacs-overlay, neovim-nightly, ...
+  }@inputs: {
+    nixosConfigurations = {
+      leonard = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };  # Pass inputs to modules
+        modules = [
+          ./hosts/leonard
+        ];
+      };
+    };
+  };
+}
